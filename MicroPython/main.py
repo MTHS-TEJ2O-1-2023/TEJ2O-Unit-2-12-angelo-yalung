a454211a46e0e9320a198573a5cdef77d419b903
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-26 17:26:50 +0000

    df

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,31 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Angelo Yalung
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from hcsr04 import HCSR04
+import machine
+import neopixel
 
 
-display.scroll("Hello, World!")
+# variables
+sensor = HCSR04(trigger_pin=1, echo_pin=2)
+np = neopixel.NeoPixel(machine.Pin(4), 4)
+distance = sensor.distance_cm()
+
+
+# if distance is greater than or equal to 10
+if distance >= 10:
+    np[0] = (0, 255, 0)
+    np[1] = (0, 255, 0)
+    np[2] = (0, 255, 0)
+    np[3] = (0, 255, 0)
+    print("Distance:", distance, "cm")
+else:
+    np[0] = (255, 0, 0)
+    np[1] = (255, 0, 0)
+    np[2] = (255, 0, 0)
+    np[3] = (255, 0, 0)
+    print("Distance:", distance, "cm")
